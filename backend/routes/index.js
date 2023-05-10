d585650e979f1bd3fb3e9179d6ad035cbbd15c32
sinisaV <sinisa.vucetic@student.um.si>
2023-05-10 23:49:38 +0200

    feat: displaying mailboxes in home page

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var mailboxController = require('../controllers/mailboxController.js');
 
+router.get('/', mailboxController.list);
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+});*/
 
 module.exports = router;
