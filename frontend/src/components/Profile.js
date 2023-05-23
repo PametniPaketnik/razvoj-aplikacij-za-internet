b047fe7705ca522502a7ae81f97183e2401625cd
sabin <sabina.pauric@student.um.si>
2023-05-23 21:39:58 +0200

    feat: added style to Profile.js

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../userContext';
 import { Navigate } from 'react-router-dom';
+import './styles/Profile.css';
 
 function Profile(){
     const userContext = useContext(UserContext); 
@@ -16,13 +17,39 @@ function Profile(){
     }, []);
 
     return (
-        <>
-            {!userContext.user ? <Navigate replace to="/login" /> : ""}
-            <h1>User profile</h1>
-            <p>Username: {profile.username}</p>
-            <p>Email: {profile.email}</p>
-            <img src={"http://localhost:3001/"+profile.path} alt={profile.username}/>
-        </>
+    <div className="user-profile">
+            <>  {!userContext.user ? <Navigate replace to="/login" /> : ""}
+                <h1>User profile</h1>
+                <img className="profile-image" src={"http://localhost:3001/" + profile.path} alt={profile.username} />
+                <div className="profile-info">
+                    <div>
+                        <span className="label">Username</span>
+                        <span className="value">{profile.username}</span>
+                    </div>
+                    <div>
+                        <span className="label">Email</span>
+                        <span className="value">{profile.email}</span>
+                    </div>
+                    <div>
+                        <span className="label">First name</span>
+                        <span className="value">{profile.firstName}</span>
+                    </div>
+                    <div>
+                        <span className="label">Last name</span>
+                        <span className="value">{profile.lastName}</span>
+                    </div>
+                    <div>
+                        <span className="label">Telephone number</span>
+                        <span className="value">{profile.tel}</span>
+                    </div>
+                    <div>
+                        <span className="label">Street and post code</span>
+                        <span className="value">{profile.street}</span>
+                        <span className="value">{profile.postcode}</span>
+                    </div>
+                </div>
+            </>
+    </div>
     );
 }
 
