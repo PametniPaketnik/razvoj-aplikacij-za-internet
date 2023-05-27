babddce26fe86d40978c094023ad909c328cbb74
mihasket <miha.sket@gmail.com>
2023-05-27 17:58:16 +0200

    feat: update profile look

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../userContext';
 import { Navigate } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import './styles/Profile.css';
 
 function Profile(){
@@ -23,29 +24,37 @@ function Profile(){
                 <img className="profile-image" src={"http://localhost:3001/" + profile.path} alt={profile.username} />
                 <div className="profile-info">
                     <div>
-                        <span className="label">Username</span>
-                        <span className="value">{profile.username}</span>
+                        <h4 className="label">Username:</h4>
+                        <h4 className="value">{profile.username}</h4>
                     </div>
                     <div>
-                        <span className="label">Email</span>
-                        <span className="value">{profile.email}</span>
+                        <h4 className="label">Email:</h4>
+                        <h4 className="value">{profile.email}</h4>
                     </div>
                     <div>
-                        <span className="label">First name</span>
-                        <span className="value">{profile.firstName}</span>
+                        <h4 className="label">First name:</h4>
+                        <h4 className="value">{profile.firstName}</h4>
                     </div>
                     <div>
-                        <span className="label">Last name</span>
-                        <span className="value">{profile.lastName}</span>
+                        <h4 className="label">Last name:</h4>
+                        <h4 className="value">{profile.lastName}</h4>
                     </div>
                     <div>
-                        <span className="label">Telephone number</span>
-                        <span className="value">{profile.tel}</span>
+                        <h4 className="label">Telephone number:</h4>
+                        <h4 className="value">{profile.tel}</h4>
                     </div>
                     <div>
-                        <span className="label">Street and post code</span>
-                        <span className="value">{profile.street}</span>
-                        <span className="value">{profile.postcode}</span>
+                        <h4 className="label">Street:</h4>
+                        <h4 className="value">{profile.street}</h4>
+                    </div>
+                    <div>
+                        <h4 className="label">Post code:</h4>
+                        <h4 className="value">{profile.postcode}</h4>
+                    </div>
+                    <div className='edit-profile'>
+                        <Link to={`/profile/edit`} className="mailbox-link">
+                            Edit profile
+                        </Link>
                     </div>
                 </div>
             </>
